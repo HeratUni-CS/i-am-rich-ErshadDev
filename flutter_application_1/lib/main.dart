e9ef73be2a6bc528a3ba7c3872b6249eea3ac0dd
ErshadDev <ershadqurbani2000@gmail.com>
2023-03-29 01:45:49 -0700

    First commit poor app

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    // ignore: prefer_const_constructors
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Ershad 98103'),
+          backgroundColor: const Color.fromARGB(255, 13, 10, 43),
+          ),
+          body: const Center(
+            child : Text('This is my first project')
+          ),
+          backgroundColor: const Color.fromARGB(255, 238, 238, 238),
+        )
+      )
+    );
+}
